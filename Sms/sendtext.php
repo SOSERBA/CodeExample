9aa9749e11830c4648baae3735b9c880754c7968
Risman Afyandi <rismanafyandi92@gmail.com>
2020-06-15 22:56:16 +0700

    update sms sample

---
+++ Sms/sendtext.php
@@ -0,0 +1,32 @@
+<?php
+
+$params = array(
+	"Identity" => "087xxxxxxxxx",
+	"Message" => "This text send from telegram"
+); 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://sms.soserba.com/users/sendtext",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params),
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Bearer TokenAnda",
+    "Content-Type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+?>
