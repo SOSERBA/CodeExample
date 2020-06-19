5ae9b29afad5f4feb2f69070663d03aeee7f499f
Risman Afyandi <rismanafyandi92@gmail.com>
2020-06-20 01:25:55 +0700

    update

---
+++ WhatsApp/authenticate.php
@@ -0,0 +1,30 @@
+<?php
+
+$params = array(
+	"username" => "08xxxxxxx",
+	"password" => "PasswordAnda"
+); 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://whatsapp.soserba.com/users/authenticate",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => json_encode($params),
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+?>
